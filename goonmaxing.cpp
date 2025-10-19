397dcb8833dcf299c457dd60a9e233f83e461902
Bothstingray350 <bubberjr1@gmail.com>
2025-10-19 13:24:51 -0700

    more changes

--- goonmaxing.cpp
+++ goonmaxing.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include "ExtraGoonFiles.h"
 
+int num1;
+int num2;
+
 int main(){
-    int num = add(1, 2);
-    std::cout << num;
+    std::cout << "Whats the fist number you want to add?" << "\n";
+    std::cin >> num1;
+    std::cout << "Whats the second number you want to add?" << "\n";
+    std::cin >> num2;
+    int num = add(num1, num2);
+    std::cout << "\n" << num1 << " + " << num2 << " = " << num;
     return 0;
 }
